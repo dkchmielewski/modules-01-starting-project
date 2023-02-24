752523cbb81205853242f1e726ee5820be66c14a
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-02-23 20:33:41 -0500

    when does module code execute?

--- assets/scripts/App/ProjectItem.js
+++ assets/scripts/App/ProjectItem.js
@@ -1,6 +1,7 @@
 import { DOMHelper } from '../Utility/DOMHelper.js';
 // import { Tooltip } from './Tooltip.js';
 
+console.log('Project Item created!');
 export class ProjectItem {
   hasActiveTooltip = false;
 
